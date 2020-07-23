d85655bbe9a43a490a0e3d27fdf24024b1eaf4af
nepp <0andrewmartin0@gmail.com>
2020-07-23 23:49:43 +0100

    impl allergy

--- src/interpret.rs
+++ src/interpret.rs
@@ -28,8 +28,8 @@ pub fn interpret(ast : &AST) {
   if let Some(&v) = env.get(&to_symbol("main")) {
     let f = unsafe { &*(v as *const Function) };
     let mut stack = [0 ; 2048];
-    let mut sf = StackFrame::new(0);
-    sf.interpret(&mut stack, f, &mut env);
+    let mut p = ProcessorState{ pc: 0, sbp: 0 };
+    interpreter_loop(&mut stack, &mut p, f, &mut env);
     println!("Execution of main function complete.");
   }
   else {
@@ -37,7 +37,7 @@ pub fn interpret(ast : &AST) {
   }
 }
   
-struct StackFrame {
+struct ProcessorState {
   /// program counter
   pc : usize,
 
@@ -45,72 +45,65 @@ struct StackFrame {
   sbp : usize,
 }
 
-impl StackFrame {
+fn reg(p : &ProcessorState, i : RegIndex) -> usize {
+  i.0 as usize + p.sbp
+}
 
-  fn new(sbp : usize) -> Self {
-    StackFrame { pc: 0, sbp }
-  }
-
-  fn reg(&self, i : RegIndex) -> usize {
-    i.0 as usize + self.sbp
-  }
-
-  fn interpret(&mut self, stack : &mut [u64], fun : &Function, env : &mut Env) {
-    loop {
-      let op = &fun.ops[self.pc];
-      println!(">>> {}:   {}", self.pc, op);
-      match op {
-        Op::Expr(r, e) => {
-          let v = match e {
-            Expr::Def(sym) =>
-              if let Some(&f) = env.get(&sym) {
-                f
-              }
-              else {
-                panic!("Symbol '{}' not present in env", sym);
-              }
-            Expr::LiteralU64(val) =>
-              *val as u64,
-            Expr::Add(a, b) =>
-              stack[self.reg(*a)] + stack[self.reg(*b)],
-          };
-          stack[self.reg(*r)] = v;
-        }
-        Op::Set(var, val) => {
-          stack[self.reg(*var)] = stack[self.reg(*val)];
-        }
-        Op::CJump{ cond, then_block, else_block } => {
-          if self.reg(*cond) != 0 {
-            self.pc = fun.blocks[then_block.0].start_op;
-          }
-          else {
-            self.pc = fun.blocks[else_block.0].start_op;
-          }
-          continue;
-        }
-        Op::Jump(block) => {
-          self.pc = fun.blocks[block.0].start_op;
-          continue;
-        }
-        Op::Arg{ index, value } => {
-          let index = self.sbp + fun.registers + (*index as usize);
-          stack[index] = stack[self.reg(*value)];
-
-        }
-        Op::Invoke(f) => {
-          let fun_address = stack[self.reg(*f)];
-          let f = unsafe { &*(fun_address as *const Function) };
-          let mut frame = StackFrame::new(self.sbp + fun.registers);
-          frame.interpret(stack, f, env);
-        }
-        Op::Debug(r) => {
-          println!("debug: {}", stack[self.reg(*r)]);
-        }
-        Op::Exit => {
-          return;
-        }
+fn interpreter_loop(stack : &mut [u64], p : &mut ProcessorState, fun : &Function, env : &mut Env) {
+  loop {
+    let op = fun.ops[p.pc];
+    println!(">>> {}:   {}", p.pc, op);
+    match op {
+      Op::Expr(r, e) => {
+        let v = match e {
+          Expr::Def(sym) =>
+            if let Some(&f) = env.get(&sym) {
+              f
+            }
+            else {
+              panic!("Symbol '{}' not present in env", sym);
+            }
+          Expr::LiteralU64(val) =>
+            val as u64,
+          Expr::Add(a, b) =>
+            stack[reg(p, a)] + stack[reg(p, b)],
+        };
+        stack[reg(p, r)] = v;
       }
-      self.pc += 1;
+      Op::Set(var, val) => {
+        stack[reg(p, var)] = stack[reg(p, val)];
+      }
+      Op::CJump{ cond, then_block, else_block } => {
+        if reg(p, cond) != 0 {
+          p.pc = fun.blocks[then_block.0].start_op;
+        }
+        else {
+          p.pc = fun.blocks[else_block.0].start_op;
+        }
+        continue;
+      }
+      Op::Jump(block) => {
+        p.pc = fun.blocks[block.0].start_op;
+        continue;
+      }
+      Op::Arg{ index, value } => {
+        let index = p.sbp + fun.registers + (index as usize);
+        stack[index] = stack[reg(p, value)];
+
+      }
+      Op::Invoke(f) => {
+        let fun_address = stack[reg(p, f)];
+        let f = unsafe { &*(fun_address as *const Function) };
+        let mut next_p = ProcessorState{ pc: 0, sbp: p.sbp + fun.registers };
+        interpreter_loop(stack, &mut next_p, f, env);
+      }
+      Op::Debug(r) => {
+        println!("debug: {}", stack[reg(p, r)]);
+      }
+      Op::Exit => {
+        return;
+      }
     }
+    p.pc += 1;
   }
 }
